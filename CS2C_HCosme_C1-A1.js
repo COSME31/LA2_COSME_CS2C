fe21d693b06eec27440edc7958b66de8289ce763
COSME, HAROLD D. <haroldcosme27@gmail.com>
2024-09-24 23:31:05 +0800

    Add files via upload

---
+++ CS2C_HCosme_C1-A1.js
@@ -0,0 +1,47 @@
+// Name
+var name = "[Harold Dulay Cosme]";
+var birthDate = "[Aplir 27, 2000]";
+var birthPlace = "[Lancuas, San Emilio, Ilocos Sur]";
+var adress = "[Lancuas, San Emilio, Ilocos Sur]";
+var course = "[Bachelor of Science in Computer Science]";
+var year = "[2]"
+var dreamjob = "[Businessman]";
+
+// Classmate1
+var name = "[Aj Cortado Lozano]";
+var birthDate = "[April 23, 2003]";
+var birthPlace = "[Calaoa-an, Candon City, Ilocos Sur]";
+var address = "[Calaoa-an, Candon City, Ilocos Sur]";
+var course = "[Bachelor of Science in Computer Science]";
+var year = "[2]";
+var dreamjob = "[Game Developer]";
+
+// Classmate2
+var name = "[Aureo Leaño Capellan Jr.]";
+var birthDate = "[April 11, 2005]";
+var birthPlace = "[San Fernando, La Union, Philippines 2500]";
+var address = "[Tamurong 2nd, Candon City, Ilocos sur]";
+var course = "[Bachelor of Science in Computer Science]";
+var year = "[2]";
+var dreamjob = "[Game Developer]";
+
+let studentName = "Harold Dulay Cosme";
+let classmate1Name = "Aj Cortado Lozano"
+let classmate2Name = "Aureo Leaño Capellan Jr."
+
+var nameRearrange = name.split(" ");
+let c1nameRearrange = name.split(" ");
+let c2nameRearrange = name.split(" ");
+
+
+let studentNameAddc1Addc2 = studentName + " " + classmate1Name + " " + classmate2Name;
+let nameAddname1Addname2 = name + " " + classmate1Name + " " + classmate2Name;
+
+console.log(name.toUpperCase() + " was born " + birthDate + " at " + birthPlace.toUpperCase() + " , and currently living at " + address.toUpperCase()+".");
+console.log(name.toLowerCase() + " is taking up " + course.toLowerCase() + " and dreams to be " + dreamjob + " after graduation.");
+
+console.log(name.toUpperCase() + " was born " + birthDate + " at " + birthPlace.toUpperCase() + " , and currently living at " + address.toUpperCase()+".");
+console.log(name.toLowerCase() + " is taking up " + course.toLowerCase() + " and dreams to be " + dreamjob + " after graduation.");
+
+console.log(name.toUpperCase() + " was born " + birthDate + " at " + birthPlace.toUpperCase() + " , and currently living at " + address.toUpperCase()+".");
+console.log(name.toLowerCase() + " is taking up " + course.toLowerCase() + " and dreams to be " + dreamjob + " after graduation.");
